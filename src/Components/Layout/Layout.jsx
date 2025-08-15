d1a791b20dd8ebcc77782bfd413beabf15ec9dc9
aqdarahmad <a.abusufyan@student.aaup.edu>
2025-08-15 11:06:38 +0300

    Add Header Component (navbar+navitem)

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
+
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer';
-import Navitem from '../Navbar/Navitem';
-import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
 
 export default function Layout() {
     return (
         <>
-          <Navbar logo="React Shop">
-        <Navitem title="Home" link="/" />
-        <Navitem title="Cart" link="/cart" />
-      </Navbar>
-      <main>
+        <Header/>
+   
         <Outlet />
-      </main>
+     
       <Footer />
       </>
     )
